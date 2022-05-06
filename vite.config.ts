523b6f2a48785111d383ba1febd01d534d793cbf
Vlad Bulgakov <stick.qwe@gmail.com>
2022-05-07 02:51:18 +0300

    add post css and svg load

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [resolve(__dirname, "./src/assets/images/sprites")],
+      // Specify symbolId format
+      symbolId: "icon-[dir]-[name]",
+    }),
+  ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
