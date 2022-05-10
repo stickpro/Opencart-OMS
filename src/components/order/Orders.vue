41419916c1919cec8037cf7366de11dc6a1c16c1
Vlad Bulgakov <stick.qwe@gmail.com>
2022-05-10 15:10:36 +0300

    import icon pack

--- src/components/order/Orders.vue
+++ src/components/order/Orders.vue
@@ -1,8 +1,33 @@
 <template>
-  <aside class="border-end border-2">
-    <div class="p-3">
-      <h2>Заказы</h2>
-
+  <aside class="vh-100 bg-white">
+    <div class="p-3 card">
+      <div class="d-flex justify-content-between">
+        <h2>Заказы</h2>
+        <button class="btn btn-ghost-dark">
+          <v-svg name="adjustments-horizontal" width="24px" />Фильтры
+        </button>
+      </div>
+      <div
+        class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last"
+      >
+        <div class="input-icon">
+          <span class="input-icon-addon">
+            <!-- Download SVG icon from http://tabler-icons.io/i/search -->
+            <v-svg name="search" width="24px" />
+          </span>
+          <input
+            type="text"
+            value=""
+            class="form-control"
+            placeholder="Поиск заказа по ID"
+            aria-label="Search in website"
+          />
+        </div>
+      </div>
     </div>
   </aside>
 </template>
+
+<script lang="ts" setup>
+
+</script>
